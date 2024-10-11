e69ef4ab3f8dcbb138f33ef8b218ca5e87aee375
Eleanor Wright <elliewright055@gmail.com>
2024-10-11 18:01:48 -0500

    Add conditionals/Switch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import conditionals.Switch;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Switch.main(args);
     }
 }
